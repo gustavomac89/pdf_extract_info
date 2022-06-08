320c064812109d25da500780b1afb352753a70fb
Gustavo Machado <gustavomac89@gmail.com>
2022-06-08 19:01:35 +0000

    Inserido arquivo para teste

--- api/controllers/lerArquivo.js
+++ api/controllers/lerArquivo.js
@@ -1,7 +1,29 @@
 
 module.exports = () => {
     const controller = {};
+    const fs = require('fs');
+    const pdf = require('pdf-parse');
+
+    let dataBuffer = fs.readFileSync('./input1.txt',{encoding:'utf8', flag:'r'});
+    let antes = "GFIP";
+    let depois = "8.40"
+    pdf(dataBuffer).then(function(data) {
+      encontraString(antes, depois, data);
+    });
+
+    encontraString = (antes, depois, texto) => {
+          if (data.text.includes(antes) && data.text.includes(depois)) {
+              let pos = data.text.lastIndexOf(palavra) + palavra.length;
+              let comp = data.text.substring(pos, pos  + 8) ;
+              console.log(comp); 
+        
+};
+    }
   
-    controller.lerArquivo = (req, res) => res.status(201).json({"resp":"teste"});  
+    controller.lerArquivo = (req, res) => res.status(201).json(
+      {"resp":"teste"}
+    
+      );  
+
     return controller;
   }
